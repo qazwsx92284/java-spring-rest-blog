a9064ab063edb35c651f8cb8f8ce5fe1c81e9692
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2021-11-22 17:10:07 -0500

    m2 updates

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,6 +1,11 @@
 package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,9 +13,109 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class PostRepository {
+public class PostRepository implements JpaRepository<Post, Long> {
     public List<Post> findAll() {
         return new ArrayList<>();
+    }
+
+    @Override
+    public List<Post> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Post> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findAllById(Iterable<Long> iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Post post) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Post> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends Post> List<S> saveAll(Iterable<S> iterable) {
+        return null;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Post> S saveAndFlush(S s) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<Post> iterable) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Post getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends Post> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Post> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Post> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Post> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Post> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Post> boolean exists(Example<S> example) {
+        return false;
     }
 
     public List<Post> saveAll(List<Post> posts) {
@@ -23,6 +128,11 @@ public class PostRepository {
 
     public Optional<Post> findById(Long id) {
         return null;
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
     }
 
     public List<Post> findByTitleContaining(String title) {
